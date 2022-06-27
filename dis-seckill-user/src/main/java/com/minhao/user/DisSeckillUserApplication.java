4b3b22d6fb1da8a09153a75cf272dc10bded62a2
minhao <hao.min@1hai.cn>
2022-06-27 18:00:21 +0800

    feat first init

---
+++ dis-seckill-user/src/main/java/com/minhao/user/DisSeckillUserApplication.java
@@ -0,0 +1,13 @@
+package com.minhao.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisSeckillUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisSeckillUserApplication.class, args);
+    }
+
+}

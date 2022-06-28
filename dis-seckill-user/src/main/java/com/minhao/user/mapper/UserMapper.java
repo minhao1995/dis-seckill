881e6dba28ffa78e0bcd61b662b31bddce20d41a
minhao <hao.min@1hai.cn>
2022-06-28 18:04:34 +0800

    feat 代码生成器

--- dis-seckill-user/src/main/java/com/minhao/user/mapper/UserMapper.java
+++ dis-seckill-user/src/main/java/com/minhao/user/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.minhao.user.mapper;
 
+import com.minhao.user.domain.User;
+
 public interface UserMapper {
+    /**
+     * 保存用户
+     * @param user
+     * @return
+     */
+    int saveUser(User user);
 }
